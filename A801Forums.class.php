2693da10ac3fdf66b63b2ac33ff4e7c3b2af3ebe
FishTFM <skyzoh.key@gmail.com>
2014-08-22 15:10:48 +0200

    #

--- A801Forums.class.php
+++ A801Forums.class.php
@@ -9,20 +9,23 @@
 class A801Forums
 {
 	// Declare and init vars.
-	private $cURL;
+	private $cURL, $cURLCookieJar;
 	private $username, $userpass;
 	private $basePath = "http://www.atelier801.com";
 
-	public function __construct($userName, $userPass)
+	public function __construct($userName = "Fish", $userPass = "Z9kYJII175NQEDYMQpkTELYhWQorahHkIqDJbEcTUdk=")
 	{
 		$this->username = $userName;
 		$this->userpass = $userPass;
 
+		$this->cURLCookieJar = tempnam("/tmp", "CURLCOOKIE");
+
 		$this->cURL = curl_init();
-		curl_setopt($this->cURL, CURLOPT_COOKIEJAR, "");
+		curl_setopt($this->cURL, CURLOPT_COOKIEJAR, $this->cURLCookieJar);
+		curl_setopt($this->cURL, CURLOPT_COOKIEFILE, $this->cURLCookieJar);
 	}
 
-	private function setCURLOpt($option, $value)
+	private function setOpt($option, $value)
 	{
 		curl_setopt($this->cURL, $option, $value);
 	}
@@ -35,7 +38,19 @@ class A801Forums
 			$this->userpass = $userPass;
 		}
 
-		setCURLOpt(CURLOPT_URL, $this->basePath . "/identification");
+		setOpt(CURLOPT_URL, $this->basePath . "/identification");
+		setOpt(CURLOPT_POST, 1);
+		setOpt(CURLOPT_POSTFIELDS, "id=" . $this->username . "&pass=" . $this->pass . "&" . $tokenName . "=" . $tokenValue);
+
+		if ($response = curl_exec($this->cURL))
+		{
+			$json = json_decode($response);
+
+			if ($json->redirection == "http://atelier801.com/index")
+				return true;
+			else
+				return false;
+		}
 	}
 
 	public function __unset()
