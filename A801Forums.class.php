813c1b070126a06f9331e6990b5a554ff6a231ef
FishTFM <skyzoh.key@gmail.com>
2014-09-01 05:00:07 +0200

    Many improvements in the connect() method :100:
    Improved the connect() method and added a todo function : disconnect().

--- A801Forums.class.php
+++ A801Forums.class.php
@@ -15,18 +15,16 @@ class A801Forums
 
 	public function __construct($cookieJar, $userName, $userPass)
 	{
+		// Initialize many fun things.
 		$this->username = $userName;
 		$this->userpass = $userPass;
 
 		$this->cURLCookieJar = $cookieJar;
-
-		$this->cURL = curl_init();
-		$this->setOpt(CURLOPT_COOKIEJAR, $this->cURLCookieJar);
-		$this->setOpt(CURLOPT_COOKIEFILE, $this->cURLCookieJar);
 	}
 
 	public function __destruct()
 	{
+		// Close the request and then unset the var.
 		curl_close($this->cURL);
 		unset($this->cURL);
 	}
@@ -37,18 +35,27 @@ class A801Forums
 	**/
 	private function setOpt($option, $value)
 	{
+		// Set a new option.
 		curl_setopt($this->cURL, $option, $value);
 	}
 
-	private function request($url, $curlOpt = null, $isPost = false, $postParams = null)
+	private function request($url, $curlOpt = null, $isPost = true, $postParams = null)
 	{
-		curl_close($this->cURL);
-
+		// Do some shitty stuff to set differents cURL options.
 		$this->cURL = curl_init();
 		$this->setOpt(CURLOPT_COOKIEJAR, $this->cURLCookieJar);
 		$this->setOpt(CURLOPT_COOKIEFILE, $this->cURLCookieJar);
+		$this->setOpt(CURLOPT_COOKIESESSION, true);
 		$this->setOpt(CURLOPT_FOLLOWLOCATION, true);
-		// If you want to add headers : $this->setOpt(CURLOPT_HTTPHEADER, array("" => ""));
+		$this->setOpt(CURLOPT_RETURNTRANSFER, true);
+		$this->setOpt(CURLOPT_HTTPHEADER, array("Host: www.atelier801.com",
+										        "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+										        "Accept: application/json, text/javascript, */*; q=0.01",
+										        "Accept-Language: fr-fr,en;q=0.5",
+										        //"Accept-Encoding: gzip, deflate",
+										        "Connection: keep-alive",
+										        "X-Requested-With: XMLHttpRequest",
+										        "Referer: http://www.atelier801.com/"));
 
 		$this->setOpt(CURLOPT_URL, $url);
 
@@ -64,13 +71,18 @@ class A801Forums
 			$this->setOpt(CURLOPT_POSTFIELDS, $postParams);
 		}
 
+		// Request it !
 		$exec = curl_exec($this->cURL);
 
+		// Some debugs informations.
 		$header_size = curl_getinfo($this->cURL, CURLINFO_HEADER_SIZE);
 		$header = substr($exec, 0, $header_size);
 		$body = substr($exec, $header_size);
 
 		return $body;
+
+		// At least, close the request.
+		curl_close($this->cURL);
 	}
 
 	/**
@@ -78,25 +90,46 @@ class A801Forums
 	**/
 	public function connect($userName = null, $userPass = null)
 	{
+		// Disconnect first to avoid problems.
+		$this->disconnect();
+
+		// If we want to connect another account than the main account.
 		if ($userName != null || $userPass != null)
 		{
 			$this->username = $userName;
 			$this->userpass = $userPass;
 		}
 
-		$indexPage = (string)$this->request($this->basePath);		
+		// Get the indexPage
+		$indexPage = (string)$this->request($this->basePath . "/forums");
 
-		/*$tokenName = $html->find("input[type=hidden]", 1)->name;
-		$tokenValue = $html->find("input[type=hidden]", 1)->value;
+		// Find the token name/value.
+		preg_match('/<input type="hidden" name="([a-zA-Z0-9 -]{1,10})" value="([a-zA-Z0-9 -]{1,50})"/', $indexPage, $matches);
+		$tokenName = $matches[1];
+		$tokenValue = $matches[2];
 
-		if ($response = $this->request($this->basePath . "/identification", null, true, "id=" . $this->username . "&pass=" . $this->userpass . "&" . $tokenName . "=" . $tokenValue))
-		{
-			$json = json_decode($response);
+		// Post fields for the login request.
+		$fields = array("id" => $this->username,
+						"pass" => $this->userpass,
+						$tokenName => $tokenValue);
 
-			if ($json->redirection == "http://atelier801.com/index")
-				return true;
-			else
-				return false;
-		}*/
+		// Let's log into.
+		$response = (string)$this->request($this->basePath . "/identification", null, true, $fields);
+		
+		/**
+		*	Bad ids: {"resultat":"ECHEC_AUTHENTIFICATION","message":"Identifiant ou mot de passe invalide."}
+		*	Good ids : {"redirection":"http://atelier801.com/index"}
+		**/
+
+		return $return;
+	}
+
+	public function disconnect()
+	{
+		/**
+		*	TODO:
+		*	Check if an user is logged and then disconnect (/deconnexion) him.
+		**/
+		return true;
 	}
 }
